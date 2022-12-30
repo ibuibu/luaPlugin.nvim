fcad84261a8c71a672b117989504d45349962f74
ibuibu <ibuibu69@gmail.com>
2022-12-30 23:53:07 +0900

    Implement single line

--- lua/luaPlugin/init.lua
+++ lua/luaPlugin/init.lua
@@ -1,19 +1,28 @@
-local function hoge()
-  local r, c = unpack(vim.api.nvim_win_get_cursor(0))
-  print('cursor', r, c)
+local function gitLine()
 
-  local result = vim.fn.systemlist('git config --get remote.origin.url')
-  for k, v in pairs(result) do
-    print(k, v)
-  end
+  local url = unpack(vim.fn.systemlist('git config --get remote.origin.url'))
+  url = url:gsub("github.com:", "github.com/")
+  url = url:gsub("git@", "https://")
+  url = url:sub(1, -5)
 
-  -- local aa = vim.fn.expand("%:p")
-  -- print(aa)
+  local commit_hash = unpack(vim.fn.systemlist('git show -s --format=%H'))
 
-  local bb = vim.api.nvim_buf_get_name(0)
-  print(bb)
+  local root_path = unpack(vim.fn.systemlist('git rev-parse --show-toplevel'))
+  local cur_path = vim.api.nvim_buf_get_name(0)
+  local file_path = cur_path:gsub(root_path, "")
+
+  local cur_line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+
+  -- local visual_first_line = vim.api.nvim_buf_get_mark(0, "<")[1]
+  -- local visual_last_line = vim.api.nvim_buf_get_mark(0, ">")[1]
+  -- print(visual_first_line, visual_last_line)
+
+  local target_url = url .. "/blob/" .. commit_hash .. file_path .. "#L" .. cur_line
+
+  vim.fn.setreg('*', target_url)
+  print("copied!")
 end
 
 return {
-  hoge = hoge
+  gitLine = gitLine
 }
