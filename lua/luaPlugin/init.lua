c67762859dc280c5a926e18a4e77a68b4ece99db
ibuibu <ibuibu69@gmail.com>
2025-01-19 15:44:46 +0900

    fix

--- lua/luaPlugin/init.lua
+++ lua/luaPlugin/init.lua
@@ -1,36 +1,32 @@
 local function shareLine()
+	local url = unpack(vim.fn.systemlist("git remote get-url origin"))
+	url = url:gsub("ssh", "https")
+	url = url:sub(1, -5)
 
-  local url = unpack(vim.fn.systemlist('git remote get-url origin'))
-  url = url:gsub("github.com:", "github.com/")
-  url = url:gsub("git@", "https://")
-  url = url:gsub("ssh", "https")
-  url = url:sub(1, -5)
+	local commit_hash = unpack(vim.fn.systemlist("git show -s --format=%H"))
 
-  local commit_hash = unpack(vim.fn.systemlist('git show -s --format=%H'))
+	local root_path = unpack(vim.fn.systemlist("git rev-parse --show-toplevel"))
+	local cur_path = vim.api.nvim_buf_get_name(0)
+	local file_path = cur_path:gsub(root_path, "")
 
-  local root_path = unpack(vim.fn.systemlist('git rev-parse --show-toplevel'))
-  local cur_path = vim.api.nvim_buf_get_name(0)
-  local file_path = cur_path:gsub(root_path, "")
+	local mode = vim.fn.mode()
 
-  local mode = vim.fn.mode()
+	local line
+	if mode == "V" then
+		local start_line = vim.fn.line("v")
+		local end_line = vim.fn.getcurpos("v")[2]
+		line = start_line .. "-" .. end_line
+		vim.api.nvim_input("<esc>")
+	else
+		line, _ = unpack(vim.api.nvim_win_get_cursor(0))
+	end
 
-  local line
-  if mode == "V" then
-    local start_line = vim.fn.line("v")
-    local end_line = vim.fn.getcurpos("v")[2]
-    line = start_line .. "-" .. end_line
-    vim.api.nvim_input("<esc>")
-  else
-    line, _ = unpack(vim.api.nvim_win_get_cursor(0))
-  end
+	local target_url = url .. "/blob/" .. commit_hash .. file_path .. "#L" .. line
 
-
-  local target_url = url .. "/blob/" .. commit_hash .. file_path .. "#L" .. line
-
-  vim.fn.setreg('*', target_url)
-  print("copied! " .. target_url)
+	vim.fn.setreg("*", target_url)
+	print("copied! " .. target_url)
 end
 
 return {
-  shareLine = shareLine
+	shareLine = shareLine,
 }
